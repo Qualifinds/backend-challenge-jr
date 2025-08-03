33e39f4190fae0deadf13a9fa430609c83c2fe3e
Raul Garcia <joragarciace@ittepic.edu.mx>
2025-08-02 18:20:01 -0700

    Submit candidate application

---
+++ Controllers/ChuckNorrisController.cs
@@ -0,0 +1,41 @@
+﻿//namespace ChuckNorrisAPI.Controllers
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("")]
+public class ChuckNorrisController : ControllerBase
+{
+    private readonly IChuckNorrisService _service;
+
+    public ChuckNorrisController(IChuckNorrisService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("categories")]
+    public async Task<IActionResult> GetCategories()
+    {
+        var categories = await _service.GetCategoriesAsync();
+        return Ok(categories);
+    }
+
+    [HttpGet("joke/{category}")]
+    public async Task<IActionResult> GetJoke(string category)
+    {
+        var categories = await _service.GetCategoriesAsync();
+        if (!categories.Contains(category))
+            return BadRequest($"Invalid category: '{category}'");
+
+        var joke = await _service.GetJokeByCategoryAsync(category);
+        if (joke == null) return NotFound();
+
+        return Ok(new
+        {
+            id = joke.Id,
+            url = joke.Url,
+            category = category,
+            value = joke.Value
+        });
+    }
+}
---
+++ Services/ChuckNorrisService.cs
@@ -0,0 +1,24 @@
+﻿using ChuckNorrisAPI.Models;
+using System.Text.Json;
+
+public class ChuckNorrisService : IChuckNorrisService
+{
+    private readonly HttpClient _http;
+
+    public ChuckNorrisService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<string>> GetCategoriesAsync()
+    {
+        var response = await _http.GetFromJsonAsync<List<string>>("https://api.chucknorris.io/jokes/categories");
+        return response ?? new List<string>();
+    }
+
+    public async Task<JokeResponse?> GetJokeByCategoryAsync(string category)
+    {
+        var url = $"https://api.chucknorris.io/jokes/random?category={category}";
+        return await _http.GetFromJsonAsync<JokeResponse>(url);
+    }
+}
